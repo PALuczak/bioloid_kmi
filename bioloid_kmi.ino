ff690c172dffb562f7c4956c71542cd7cc225978
Tomek Jaworski <tjaworski@iis.p.lodz.pl>
2015-10-19 17:17:09 +0200

    ADDED: some new methods for building and sending messages to ax servo

--- bioloid_kmi.ino
+++ bioloid_kmi.ino
@@ -152,6 +152,12 @@ void setup() {
 	//USART_Init(8); // 115.2k
 	//ATMEL manual: p208, p191
 
+	/* nie musisz za każdym razem ustawiać prędkości transmisji.
+	  zobacz w dokumentacji, że parametr 4 jest w obszarze pamięci EEPROM,
+	  a zatem wartość predkości transmisji jest pamiętana po ponownym włączeniu serwonapędu.
+	  Co wiecej, takie częste zapisaywanie parametrów do EEPROMa spowoduje jego uszkodzenie (wypalenie).
+	 */
+	
 	// set baud in servo
 	USART_Transmit(0xFF);
 	USART_Transmit(0xFF); // header
@@ -164,22 +170,125 @@ void setup() {
 	USART_Transmit(0x24); // checksum
 }
 
+uint8_t tx_buffer[32] = {0xFF, 0xFF};
+uint8_t* tx_buffe_write_ptr = NULL;
+
+enum class AXInstruction : uint8_t
+{
+	Ping = 0x01,
+	ReadData = 0x02,
+	WriteData = 0x03,
+	RegWrite = 0x04,
+	Action = 0x05,
+	Reset = 0x06,
+	SyncWrite = 0x83
+};
+
+enum class AXParameter : uint8_t
+{
+	// ............
+	
+	ID = 0x03,
+	BaudRate = 0x04,
+	
+	GoalPosition_LoByte = 0x1e,
+	GoalPosition_HiByte = 0x1f,
+	
+	// .............
+};
+
+
+inline static void ax_begin_message(uint8_t dev_id, AXInstruction instruction)
+{
+	tx_buffer[2] = dev_id;
+	//tx_buffer[3] = 0;
+	tx_buffer[4] = (uint8_t)instruction;
+	tx_buffe_write_ptr = tx_buffer + 5;
+	return tx_buffe_write_ptr;
+}
+
+inline static void ax_message_set_parameter(AXParameter param, uint8_t value)
+{
+	tx_buffe_write_ptr[0] = (uint8_t)param;
+	tx_buffe_write_ptr[1] = value;
+	tx_buffe_write_ptr += 2;
+}
+
+uint8_t ax_end_message(void)
+{
+	// okresl dlugosc pakietu
+	tx_buffer[3] = 2 + tx_buffe_write_ptr - (tx_buffer + 5);
+	
+	// policz sumę kontrolną i dopisz na koncu wiadomosci
+	uint8_t crc = 0;
+	uint8_t* ptr = tx_buffer + 2;
+	uint8_t len = tx_buffer[3] + 2;
+	while(len > 0)
+		crc += tx_buffer[];
+	*tx_buffe_write_ptr = (uint8_t)~crc;
+	
+	
+	// zwroc CAŁKOWITĄ długość bufora do wysłania
+	return (tx_buffe_write_ptr - tx_buffer) + 1;
+}
+
+void ax_send_message(void)
+{
+	uint8_t len = tx_buffer[3] + 4;
+	uint8_t* ptr = tx_buffer;
+	
+	while(len > 0)
+		USART_Transmit(*ptr++);
+}
+
+
 void loop() {
 	//USART_Transmit('2');
 	//TXRX_SendString("Test String, please ignore\n\r");
 	//TXRX_SendHex(15);
 	// write
+	
+	/* ok, ale zanim ustawisz pozycję docelową to pytanie:
+	   w jakim trybiej jest ta pozycja?? czy to ma być tryb położenia czy prędkości?
+	   Skąd wiesz, w jakim trybie jest teraz serwo.
+	 */
+	
+	
 	USART_Transmit(0xFF);
 	USART_Transmit(0xFF); // header
 	USART_Transmit(0x01); // id
 	USART_Transmit(0x06); // length
 	USART_Transmit(0x03); // instruction
-	USART_Transmit(0x30); // goal position low
-	USART_Transmit(0x93); // value
+	USART_Transmit(0x30); // goal position low	<<<<<<<<<< to nie jest adres goal position low tylko Punch Low
+	USART_Transmit(0x93); // value				<<<<<<<<<< i punch High
 	USART_Transmit(0x31); // goal position high
 	USART_Transmit(0x00); // value
 	USART_Transmit(0x01); // checksum
 
+	
+	// jeszcze raz to samo, tylko w ludziej postaci :)
+	uint16_t position = 0x93;
+	
+	ax_begin_message(0x01, AXInstruction::WriteData);
+	ax_message_set_parameter(AXParameter::GoalPosition_LoByte, position & 0xFF);
+	ax_message_set_parameter(AXParameter::GoalPosition_HiByte, (position >> 8) & 0xFF);
+	ax_end_message();
+	
+	ax_send_message();
+	
+
+	/*
+	 brakuje mi kodu
+	 
+	 tak - AX12 wymaga łącza half duplex. Brakuje kodu, który sterowałby przełączenikiem TTL od kierunku transji
+	 to ma działać tak:
+	 Pin sterujący ustawiasz wtryb Arduino --> serwo i odpalasz ax_send_message()
+	 Zaraz po zakoczeniu wewnętrznej petli pin sterujący nalezy przełączyć w tryb arduino <-- serwo i rozpoczac oczekiwanie na dane.
+	 Przy czym oczekiwanie musi być podparte czasem (timeout) lub przerwaniem i czasem (najlepsze, ale bardziej złożone rozwiązanie)
+	 
+	*/
+	
+	
 	// the above attempt to comunicate with servo failed
 	// possibly due to bad baudrate or another unknown problem
 	delay(1000);
