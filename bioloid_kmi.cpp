5ab68b3677d3fc7de91c9c32d1f7b939dd581b37
PALuczak <piotr.andrzej.luczak@gmail.com>
2015-11-24 02:51:32 +0100

    ADDED: timer and associated interrupt, USART RX interrupt

--- bioloid_kmi.cpp
+++ bioloid_kmi.cpp
@@ -1,24 +1,51 @@
 # include <avr/io.h>
 # include <avr/interrupt.h>
 
-uint8_t TXpacket [259] = {0xFF, 0xFF}; // as maximum length is 0xFF + checksum etc.
+# define MAX_BFR_LEN 259
+
+uint8_t TXpacket [MAX_BFR_LEN] = {0xFF, 0xFF}; // as maximum length is 0xFF + checksum etc.
 uint8_t TXlength; // number of parameters + 2
 uint8_t TXID; // from 0 to 253, 254 is BROADCAST_ID
 uint8_t TXinstruction;
-uint8_t RXbuffer[259];
-uint8_t RXlength;
-uint8_t *RXbufferptr=RXbuffer;
+volatile uint8_t RXbuffer[MAX_BFR_LEN];
+uint8_t RXlength; // volatile?
+volatile uint8_t *RXbufferptr = RXbuffer;
 
 ISR(USART1_RX_vect) { // USART receive
-	*RXbufferptr = UDR1;
+	*RXbufferptr = UDR1; // reding UDR clears interrupt flag
 	RXbufferptr++;
-	//reset timer
+	TCNT1 = 0; // reset timer 1
 }
 
-ISR(TIMER0_COMPA_vect) {
-	// check buffer
-	// clear buffer
-	// reset timer
+ISR(TIMER1_COMPA_vect) {
+	// TODO: check buffer
+	// TODO: if ok: stop timer, leave interrupt
+	// else:
+	do {
+		*RXbufferptr = 0;
+	} while (RXbufferptr > RXbuffer);
+	TCNT1 = 0; // reset timer 1
+}
+
+void CPU_SetInterruptFlag() { // unused as avr lib has sei() implemented
+	unsigned char sreg = 0;
+	sreg = SREG;
+	sreg = sreg | 0b10000000; // set the I bit
+	SREG = sreg;
+}
+
+void CPU_EnableRXInterrupt () {
+	/*unsigned char reg = 0;
+	reg = UCSR1B;
+	reg = reg | 0b10000000; // set RXCIE1 bit
+	UCSR1B = reg;*/
+	UCSR1B |= (1 << RXCIE1);
+}
+
+void CPU_SetUpTimer() {
+	TCCR1B |= ((1 << CS11) | (1 << CS12)); // set up prescaler = 64 (p.134 manual)
+	OCR1A = 250 * 100; // 250 = 1 ms // set up output compare value for interrupt
+	TIMSK1 |= (1 << OCIE1A); // enable interrupt on clock compare
 }
 
 void USART_Init( unsigned int baud ) {
@@ -117,43 +144,38 @@ public:
 	};
 };
 
-void TXSetLength(uint8_t length) {
+void TX_SetLength(uint8_t length) {
 	TXlength = length;
 }
 
-void TXSetId(uint8_t ID) {
+void TX_SetId(uint8_t ID) {
 	TXID = ID;
 }
 
-void TXSetInstruction(AXInstruction::Type instruction) {
+void TX_SetInstruction(AXInstruction::Type instruction) {
 	TXinstruction = instruction;
 }
 
-void TXSetParameter(int index, AXParameter::Type parameter) {
+void TX_SetParameter(int index, AXParameter::Type parameter) {
 	if (index >= 253) return;
 	TXpacket[index] = parameter;
 }
 
-int RXReceivePacket() {
+void DXL_Initialize() {
+	USART_Init(0); // 1 000 000 baud
+	for (int i = 0; i < MAX_BFR_LEN; i++) {
+		RXbuffer[i] = 0;
+		TXpacket[i] = 0;
+	}
+	CPU_SetUpTimer();
+	CPU_EnableRXInterrupt ();
+	sei(); // enable interrupts
 
 }
-
 
 int main () {
-	// enable interrupts
-	unsigned char sreg = 0;
-	sreg = SREG;
-	sreg = sreg | 0b10000000;
-	SREG = sreg;
-
-	// setup timer
-	/*
-	OCR0A, OCR0B - output compare registers
-	OCIE0A, OCIE0B - interrupt enable (TIMSK0)
-	timer 1 and 3 are 16 bit
-	timer 0 is 8 bit
-	16MHz clock sppeed
-	*/
-
-
+	for(;;); // keep program running
 }
+
+// current timer implemantation could be replaced with CTC timer (p.133)
+// CTC timer could interrupt and incremant volatile uint8_t -> move from 16bit to 8 bit timer
